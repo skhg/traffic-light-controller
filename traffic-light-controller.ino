144ef44c609ba712c531c4cc9a780dd1a8cef178
Jack <git@jackhiggins.ie>
2020-06-04 15:26:53 +0200

    add some stuff for the apple home connection

--- traffic-light-controller.ino
+++ traffic-light-controller.ino
@@ -1,6 +1,10 @@
-#define RED_LIGHT D1
-#define GREEN_LIGHT D2
+#define RED_LIGHT D5
+#define GREEN_LIGHT D6
 
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -50,10 +54,12 @@ String renderPage() {
     </head>\
     <body>\
       <h1>RED Light</h1>\
+      <p>Relay 1</p>\
       <form method=\"post\" action=\"" + redFormAction + "\">\
         <input type=\"submit\" value=\"" + redButton + "\">\
       </form>\
       <h1>GREEN Light</h1>\
+      <p>Relay 2</p>\
       <form method=\"post\" action=\"" + greenFormAction + "\">\
         <input type=\"submit\" value=\"" + greenButton + "\">\
       </form>\
@@ -91,7 +97,7 @@ void handlePostLightControl(int light, boolean newState){
       case GREEN_LIGHT: _greenLit = newState; break;
     }
     
-    digitalWrite(light, newState);
+    digitalWrite(light, !newState); // because LOW means ON
     Serial.println(String(light) + " is now "+ String(newState));
 
     String content = renderPage();
@@ -101,24 +107,29 @@ void handlePostLightControl(int light, boolean newState){
 }
 
 void redOn(){
-  handlePostLightControl(RED_LIGHT, HIGH);
+  handlePostLightControl(RED_LIGHT, true);
 }
 
 void greenOn(){
-  handlePostLightControl(GREEN_LIGHT, HIGH);
+  handlePostLightControl(GREEN_LIGHT, true);
 }
 
 void redOff(){
-  handlePostLightControl(RED_LIGHT, LOW);
+  handlePostLightControl(RED_LIGHT, false);
 }
 
 void greenOff(){
-  handlePostLightControl(GREEN_LIGHT, LOW);
+  handlePostLightControl(GREEN_LIGHT, false);
 }
 
 void setup(void) {
+  homekit_server_reset();
+  
   pinMode(RED_LIGHT, OUTPUT);
   pinMode(GREEN_LIGHT, OUTPUT);
+
+  digitalWrite(RED_LIGHT, HIGH);
+  digitalWrite(GREEN_LIGHT, HIGH);
   
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -152,8 +163,61 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+
+  my_homekit_setup();
 }
 
 void loop(void) {
   server.handleClient();
+  my_homekit_loop();
+}
+
+//==============================
+// HomeKit setup and loop
+//==============================
+
+// access your HomeKit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_switch_on;
+
+static uint32_t next_heap_millis = 0;
+
+#define PIN_SWITCH 2
+
+//Called when the switch value is changed by iOS Home APP
+void cha_switch_on_setter(const homekit_value_t value) {
+  bool on = value.bool_value;
+  cha_switch_on.value.bool_value = on;  //sync the value
+  LOG_D("Switch: %s", on ? "ON" : "OFF");
+  digitalWrite(PIN_SWITCH, on ? LOW : HIGH);
+}
+
+void my_homekit_setup() {
+  pinMode(PIN_SWITCH, OUTPUT);
+  digitalWrite(PIN_SWITCH, HIGH);
+
+  //Add the .setter function to get the switch-event sent from iOS Home APP.
+  //The .setter should be added before arduino_homekit_setup.
+  //HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
+  //Maybe this is a legacy design issue in the original esp-homekit library,
+  //and I have no reason to modify this "feature".
+  cha_switch_on.setter = cha_switch_on_setter;
+  arduino_homekit_setup(&config);
+
+  //report the switch value to HomeKit if it is changed (e.g. by a physical button)
+  //bool switch_is_on = true/false;
+  //cha_switch_on.value.bool_value = switch_is_on;
+  //homekit_characteristic_notify(&cha_switch_on, cha_switch_on.value);
+}
+
+void my_homekit_loop() {
+  arduino_homekit_loop();
+  const uint32_t t = millis();
+  if (t > next_heap_millis) {
+    // show heap info every 5 seconds
+    next_heap_millis = t + 5 * 1000;
+    LOG_D("Free heap: %d, HomeKit clients: %d",
+        ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+  }
 }
