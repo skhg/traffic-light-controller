8e0ac77135ef1a1e584287512b90b46050e56961
Jack <git@jackhiggins.ie>
2020-06-08 21:45:09 +0200

    add non-functional party mode button, endpoint for tempo, and remove homepod stuff

--- traffic-light-controller.ino
+++ traffic-light-controller.ino
@@ -10,6 +10,7 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <home_wifi.h>
+#include <ArduinoJson.h>
 
 const String HOST_NAME = "traffic-light";
 const char* ssid     = WIFI_SSID;
@@ -19,9 +20,11 @@ const String _falseString = "false";
 const String _trueString = "true";
 
 ESP8266WebServer server(80);
+StaticJsonDocument<200> doc;
 
 boolean _redLit = false;
 boolean _greenLit = false;
+int _bpm = 100;
 
 String renderPage() {
 
@@ -124,8 +127,26 @@ void greenOff(){
   handlePostLightControl(GREEN_LIGHT, false);
 }
 
+void setTempo(){
+  if (server.method() != HTTP_POST) {
+    server.send(405, "text/plain", "Method Not Allowed");
+    return;
+  }
+
+  if(server.hasArg("plain") == false) {
+    server.send(400, "text/plain", "Missing body");
+    return;
+  }
+    
+  deserializeJson(doc, server.arg("plain"));
+  _bpm = doc["bpm"];
+
+  Serial.println("BPM is now: " + String(_bpm));
+  
+  server.send(204, "text/html", "");
+}
+
 void setup(void) {
-  homekit_server_reset();
   
   pinMode(RED_LIGHT, OUTPUT);
   pinMode(GREEN_LIGHT, OUTPUT);
@@ -160,67 +181,15 @@ void setup(void) {
   server.on("/red/off", redOff);
   server.on("/green/on", greenOn);
   server.on("/green/off", greenOff);
+  server.on("/tempo", setTempo);
   server.on("/status", lightStatus);
   
   server.onNotFound(handleNotFound);
 
   server.begin();
   Serial.println("HTTP server started");
-
-  my_homekit_setup();
 }
 
 void loop(void) {
   server.handleClient();
-  my_homekit_loop();
-}
-
-//==============================
-// HomeKit setup and loop
-//==============================
-
-// access your HomeKit characteristics defined in my_accessory.c
-extern "C" homekit_server_config_t config;
-extern "C" homekit_characteristic_t cha_switch_on;
-
-static uint32_t next_heap_millis = 0;
-
-#define PIN_SWITCH 2
-
-//Called when the switch value is changed by iOS Home APP
-void cha_switch_on_setter(const homekit_value_t value) {
-  bool on = value.bool_value;
-  cha_switch_on.value.bool_value = on;  //sync the value
-  LOG_D("Switch: %s", on ? "ON" : "OFF");
-  digitalWrite(PIN_SWITCH, on ? LOW : HIGH);
-}
-
-void my_homekit_setup() {
-  pinMode(PIN_SWITCH, OUTPUT);
-  digitalWrite(PIN_SWITCH, HIGH);
-
-  //Add the .setter function to get the switch-event sent from iOS Home APP.
-  //The .setter should be added before arduino_homekit_setup.
-  //HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
-  //Maybe this is a legacy design issue in the original esp-homekit library,
-  //and I have no reason to modify this "feature".
-  cha_switch_on.setter = cha_switch_on_setter;
-  arduino_homekit_setup(&config);
-
-  //report the switch value to HomeKit if it is changed (e.g. by a physical button)
-  //bool switch_is_on = true/false;
-  //cha_switch_on.value.bool_value = switch_is_on;
-  //homekit_characteristic_notify(&cha_switch_on, cha_switch_on.value);
-}
-
-void my_homekit_loop() {
-  arduino_homekit_loop();
-  const uint32_t t = millis();
-  if (t > next_heap_millis) {
-    // show heap info every 5 seconds
-    next_heap_millis = t + 5 * 1000;
-    LOG_D("Free heap: %d, HomeKit clients: %d",
-        ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
-
-  }
 }
